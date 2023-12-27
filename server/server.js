f45b6eeaf523c809cf8284822058b5c26bdbd5f6
demtomi <demeter.tamas.ev@gmail.com>
2023-12-27 18:28:16 +0100

    feat: server.js created ^--^  ^------------^ |     | |     +-> client folder and server.js created, mongodb connection, cors established

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+
+const { MONGO_URL, PORT = 8080 } = process.env;
+
+if (!MONGO_URL) {
+    console.error("Missing MONGO_URL environment variable");
+    process.exit(1);
+};
+
+const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use(express.json());
+
+
+const main = async () => {
+    await mongoose.connect(MONGO_URL);
+    console.log("DB Connected");
+
+    app.listen(PORT, () => {
+        console.log("App is listening on 8080");
+    });
+};
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
