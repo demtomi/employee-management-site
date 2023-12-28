e1d4717c86c86a69d0b8948b97f0b798b705792d
demtomi <demeter.tamas.ev@gmail.com>
2023-12-28 17:17:57 +0100

    fix: mongo connection removed, router route fixed ^--^  ^------------^ |     | |     +-> mongodb connection removed, it will use the mongo image from docker, employee and equipment route fixed - variable name was wrong

--- server/server.js
+++ server/server.js
@@ -1,17 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require('cors');
 
-const employeeRouter = require("./routes/employees");
-const equipmentRouter = require('./routes/equipment');
+const employeeRouter = require("./routes/employee_route");
+const equipmentRouter = require('./routes/equipment_route');
 
-const { MONGO_URL, PORT = 8080 } = process.env;
-
-if (!MONGO_URL) {
-    console.error("Missing MONGO_URL environment variable");
-    process.exit(1);
-};
+const PORT = 8080
 
 const app = express();
 app.use(cors({ origin: "http://localhost:8080" }));
@@ -21,8 +15,6 @@ app.use("/api/employees", employeeRouter);
 app.use("/api/equipment", equipmentRouter);
 
 const main = async () => {
-    await mongoose.connect(MONGO_URL);
-    console.log("DB Connected");
 
     app.listen(PORT, () => {
         console.log("App is listening on 8080");
