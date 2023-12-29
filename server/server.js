373b385fefd7e38851c305c23f74a96f0569db2e
demtomi <demeter.tamas.ev@gmail.com>
2023-12-29 16:30:31 +0100

    feat: new server.js file ^--^  ^------------^ |     | |     +-> server.js file completly erased and rebuilt to match new connection to docker based mongodb, also no routes used in this version

--- server/server.js
+++ server/server.js
@@ -1,39 +1,36 @@
 const express = require("express");
 const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient
-
-const employeeRouter = require("./routes/employee_route");
-const equipmentRouter = require('./routes/equipment_route');
+const { MongoClient } = require('mongodb')
 
 const PORT = 5000;
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000" }));
-
 app.use(express.json());
 
+const uri = 'mongodb://admin:pass@localhost:27017';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 //GET EMPLOYEE
-app.get("/api/employees", async (req, res) => {
-    const response = res;
+app.get('/employees', async (req, res) => {
+    try {
+        await client.connect();
 
-    MongoClient.connect('mongodb://admin:pass@localhost:27017', function (err, client) {
-        if (err) throw err;
+        const database = client.db('employees');
+        const collection = database.collection('employee');
 
-        const db = client.db('employees');
-        const query = { userid: 1 };
-        db.collection('employee').findOne(query, function (err, result) {
-            if (err) throw err;
+        const employees = await collection.find({}).toArray();
 
-            client.close();
-            response.send(result);
-            console.log(result)
-        })
-    });
+        res.json(employees);
+    } catch (error) {
+        console.error('Error fetching employees:', error);
+        res.status(500).send('Internal Server Error');
+    } finally {
+        // Close the connection
+        client.close();
+    }
 });
 
-// app.use("/api/employees", employeeRouter);
-app.use("/api/equipment", equipmentRouter);
-
 app.listen(PORT, () => {
-    console.log(`App is listening on ${PORT}`);
+    console.log(`Server is listening on ${PORT}`);
 });
