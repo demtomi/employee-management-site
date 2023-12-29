43312bc297dc8a9187e4db73d4c9477a5baa521a
demtomi <demeter.tamas.ev@gmail.com>
2023-12-29 17:20:28 +0100

    feat: app.js conditional rendering ^--^  ^------------^ |     | |     +-> app.js gives the option to render the main,equipment or employees page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,52 @@
-// App.js
-import React from 'react';
-import EmployeePage from './EmployeePage';
+import React, { useState } from 'react';
+import './App.css';
+import EmployeePage from './components/EmployeePage';
+import EquipmentPage from './components/EquipmentPage';
+import MainPage from './components/MainPage';
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('main');
+
+  const renderPage = () => {
+    switch (selectedPage) {
+      case 'employees':
+        return (
+          <div className="employees-page">
+            <EmployeePage />
+          </div>
+        );
+
+      case 'equipment':
+        return (
+          <div className="equipments-page">
+            <EquipmentPage />
+          </div>
+        );
+
+      default:
+        return (
+          <div className="main-page">
+            < MainPage />
+          </div>
+        );
+    }
+  };
+
+  const handlePageChange = (page) => {
+    setSelectedPage(page);
+  };
+
   return (
-    <div className="App">
-      <EmployeePage />
+    <div className='App'>
+      <div className="dropdown">
+        <button className="dropbtn">Select Page</button>
+        <div className="dropdown-content">
+          <button onClick={() => handlePageChange('main')}>Main Page</button>
+          <button onClick={() => handlePageChange('employees')}>Employees</button>
+          <button onClick={() => handlePageChange('equipment')}>Equipment</button>
+        </div>
+      </div>
+      {renderPage()}
     </div>
   );
 }
